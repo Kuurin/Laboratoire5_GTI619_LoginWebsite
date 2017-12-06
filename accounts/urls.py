81cd83478ed77510a1b0d6f71629f0876571d728
Amélie Nguyen <tuong_vi06@hotmail.com>
2017-12-05 22:30:59 -0500

    logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth.views import login,logout
 
 urlpatterns = [
     url(r'^(?:home)?$', views.home),
-    url(r'^login/$', login, {'template_name': 'accounts/login.html'}),
-    url(r'^logout/$', logout, {'template_name': 'accounts/logout.html'}),
+    url(r'^login/$', login, {'template_name': 'accounts/login.html'}, name='login'),
+    url(r'^logout/$', logout, {'template_name': 'accounts/logout.html'}, name='logout')
 ]
 
 #user authentification urls
